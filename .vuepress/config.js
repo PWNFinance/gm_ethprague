b412b742dbb11fcfe8bb015d49089596fe8a2b17
Jan Miksik <jan.miksik@email.cz>
2023-04-06 13:26:34 +0200

    update template

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,34 +1,83 @@
 module.exports = {
-  // Use the name of the repo if the content is served from GitHub Pages
-  // base: '/PragueBlockchainWeek/',
-  // Leave the base as '/' if serving the content from a custom domain
-  base: '/',
-  title: 'Amsterdam GM Events',
+  base: "/",
+  // title: "Rio GM Events",
+  // heading: "GM Rio 2023",
+  // dateSubheading: "22. March - 3. April 2023",
+  // subHeading:
+  //   "We're bringing the Ethereum community to Rio de Janeiro! GM events website shows you events happening in Rio and allows anyone to add and update their side event.",
+  // calendarHeading: "March 2023",
+  // submitEventLink: "https://gmevents.typeform.com/riodejaneiro",
+  startDate: {
+    day: 22,
+    month: 3,
+    year: 2023,
+  },
+  endDate: {
+    day: 3,
+    month: 4,
+    year: 2023,
+  },
+  daysInFirstMonth: 31,
+  startHour: 8,
+  endHour: 24,
+  duarationInDays: 13,
+  dayNames: [
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+    "Monday",
+  ],
+  layout: 0, // 0 - banner layout, 1 - venue layout
+  bannerPath: "TwitHeader_1500x5002.jpg", // only if banner layout
+  venues: [
+    {
+      name: "Bamba House - Catete, Rio de Janeiro",
+      desc: "EthSamba Hack will happen at the Centro de Convenções Florida located in Catete one of the historic districts of Rio de Janeiro and with access to the attached Museum of the Republic which has some of the nicest open gardens in the city. A surprise Hacker House will be announced soon!",
+      path: "EthSamba_venue.jpg",
+    },
+    {
+      name: "ETHSamba Hacker House",
+      desc: "The Hacker House of EthSamba that will be open 24 hours from the start of the Hackathon. In it, all you need is to build your best project and take quick breaks by jumping in the pool or admiring the view!",
+      path: "ethsambahack.jpeg",
+    },
+    
+  ], // only if venue layout
   head: [
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['link', { rel: 'icon', href: '/logo-black.png' }],
-    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    ["link", { rel: "icon", href: "./logo_gm_events.svg" }],
+    [
+      "meta",
+      {
+        name: "viewport",
+        content: "width=device-width,initial-scale=1,user-scalable=no",
+      },
+    ],
   ],
   themeConfig: {
     nav: [
       {
-        text: 'Events',
-        link: '/events/'
+        text: "Home",
+        link: "/",
       },
       {
-        text: 'Calendar',
-        link: '/calendar/'
+        text: "Events",
+        link: "/events/",
       },
       {
-        text: 'ETHDam',
-        link: 'https://www.ethdam.com/'
+        text: "Calendar",
+        link: "/calendar/",
       },
-      {
-        text: 'Submit Event',
-        link: 'https://gmevents.typeform.com/amsterdam'
-      }
     ],
-    logo: 'logo.png',
+    logo: "logo_gm_events.svg",
     search: false,
     // Assumes GitHub. Can also be a full GitLab url.
     // repo: 'JosefJ/PragueBlockchainWeek',
@@ -41,10 +90,10 @@ module.exports = {
     // if your docs are not at the root of the repo:
     // docsDir: 'docs',
     // if your docs are in a specific branch (defaults to 'master'):
-    docsBranch: 'master',
+    docsBranch: "master",
     // defaults to false, set to true to enable
-    editLinks: true
+    editLinks: true,
     // custom text for edit link. Defaults to "Edit this page"
     // editLinkText: 'Help us improve this page!'
-  }
-}
+  },
+};
--- README.md
+++ README.md
@@ -5,20 +5,17 @@ actionLink: /events/
 
 description: GM Week initiatives are happening all over the world whenever
              there is a large gathering of blockchain and cryptocurrencies enthusiasts
-             around. The Amsterdam GM is no different.
+             around. The Rio GM is no different.
              This is a voluntary initiative which was formed around the ETHPrague 2022
              and which will provide an overview and support for all the blockchain
-             technology related events happening in Amsterdam between May 15th and May 21th 2023.
+             technology related events happening in Rio between March 22nd and April 3th 2023.
 
-tagline: 15. - 21. May 2023
+tagline: 22. March - 3. April 2023
 
 features:
 - title: Paralelni Polis
   details: The unique crypto-only hacker base which is facilitating a 
     discussion platform for topics related to tech, art and science.
-
-- title: Gabriel Loci
-  details: Funky place in Smichov
 
   
 footer:
@@ -37,10 +34,10 @@ footer:
 
 GM Week initiatives are happening all over the world whenever
 there is a large gathering of blockchain and cryptocurrencies enthusiasts
-around. The Amsterdam GM is no different.
+around. The Rio GM is no different.
 This is a voluntary initiative which was formed around the ETHPrague 2022
 and which will provide an overview and support for all the blockchain
-technology related events happening in Amsterdam between May 15th and May 21th 2023.
+technology related events happening in Rio between March 22nd and April 3th 2023.
 
 Feel free to reach out contact@pwn.xyz if you have any questions.
 <br />
